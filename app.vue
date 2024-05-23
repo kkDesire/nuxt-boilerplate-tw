de529bc8ffbf850beeb3092b31649c8c8ea6c483
KOUASSI Désiré <desire.kouassi@mobisoft.ci>
2024-05-23 12:34:56 +0000

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-black dark:text-gray-300">
+      Boilterplate with tw an @antfu/eslint-config@latest
+    </h1>
+  </div>
+</template>
+
+<style>
+html,
+body {
+  @apply bg-gray-100 dark:bg-gray-900;
+}
+</style>
